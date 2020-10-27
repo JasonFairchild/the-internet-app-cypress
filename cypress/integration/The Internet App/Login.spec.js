e627e1a0e831a93d9c5c96d5c2d619875f1778a5
Jason Fairchild <56280950+JasonFairchild@users.noreply.github.com>
2020-10-27 11:45:39 -0600

    Update Login.spec.js

--- cypress/integration/The Internet App/Login.spec.js
+++ cypress/integration/The Internet App/Login.spec.js
@@ -6,15 +6,21 @@ describe("Login page", () => {
   });
 
   it("Verifies Login greeting", () => {
-    
+    cy.findByRole('heading', { name: /Login Page/ });
   });
 
   it("Verifies invalid username", () => {
-    
+    cy.findByRole('textbox', { name: /Username/ })
+      .type('Wrong {enter}');
+    cy.findByText('Your username is invalid!');
   });
 
-  it("Verifies invalid password", () => {
-    
+  it.only("Verifies invalid password", () => {
+    cy.findByRole('textbox', { name: /Username/ })
+      .type('tomsmith');
+    cy.findByLabelText('Password')
+      .type('invalid {enter}');
+    cy.findByText('Your password is invalid!');
   });
 
   it("Verifies successful login", () => {
