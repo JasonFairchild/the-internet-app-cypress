855bba97b75a66e4a4adf0343cafc5a76047ee7e
Jason Fairchild <56280950+JasonFairchild@users.noreply.github.com>
2020-10-26 19:16:23 -0600

    Update Add-Remove-Elements.spec.js

--- cypress/integration/The Internet App/Add-Remove-Elements.spec.js
+++ cypress/integration/The Internet App/Add-Remove-Elements.spec.js
@@ -6,10 +6,29 @@ describe('Add/Remove elements page', () => {
     });
   
     it('Verifies section header', () => {
-      
+      cy.findByRole('heading', { name: /Add\/Remove Elements/});
     });
 
     it('Verifies add/remove for two items', () => {
-      cy.findByRole('button', {name: /Add/})
+      cy.findByRole('button', { name: /Add Element/ })
+        .click();
+      cy.findAllByRole('button', { name: /Delete/ })
+        .should('have.lengthOf', 1);
+
+      cy.findByRole('button', { name: /Add Element/ })
+        .click();
+      cy.findAllByRole('button', { name: /Delete/ })
+        .should('have.lengthOf', 2);
+
+      cy.findAllByRole('button', { name: /Delete/ })
+        .eq(1)
+        .click();
+      cy.findAllByRole('button', { name: /Delete/ })
+        .should('have.lengthOf', 1);
+
+      cy.findAllByRole('button', { name: /Delete/ })
+        .click();
+      cy.findAllByRole('button', { name: /Delete/ })
+        .should('not.exist');
   });
 });
