138f0e0f36d9fdae93a1b5a0bc0fc686612fd50c
Jason Fairchild <56280950+JasonFairchild@users.noreply.github.com>
2020-10-27 11:27:45 -0600

    Starting again

--- cypress/integration/The Internet App/Phone.spec.js
+++ cypress/integration/The Internet App/Phone.spec.js
@@ -8,16 +8,19 @@ describe("Phone number page without country select", () => {
   // Generally I would test phone validation at a lower level, not with E2E tests so much
   // I used cypress's playground selector to get generic selectors since good ones are sparse on this page
   it("Summary - Valid country code", () => {
+    // Find the Summary phone field and type into it
     cy.get(':nth-child(2) > form > input')
       .type('18013731448');
+    // Verify the value display field contains the correct value
     cy.contains('+18013731448');
   });
 
   it("Summary - Invalid country code", () => {
     cy.get(':nth-child(2) > form > input')
       .type('8013731448');
+    // Find value display and verify it doesn't display with no valid country code
     cy.get(':nth-child(3) > :nth-child(2) > :nth-child(5) > code')
-      .should('not.have.value', '+18013731448');
+      .should('not.have.value', '8013731448');
   });
 
   it("Country code select menu", () => {
